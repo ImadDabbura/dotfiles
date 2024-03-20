bf9c4f2ef186e2ac03ec5f50200a0ac0048dcc92
ImadDabbura <imad.dabbura@hotmail.com>
2024-03-19 19:25:52 -0500

    vim: Replace packer with LazyVim

--- nvim/config/lua/user/plugins.lua
+++ nvim/config/lua/user/plugins.lua
@@ -1,41 +1,53 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
     "git",
     "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]]
+-- vim.cmd [[
+--   augroup packer_user_config
+--     autocmd!
+--     autocmd BufWritePost plugins.lua source <afile> | PackerSync
+--   augroup end
+-- ]]
 
--- Use a protected call so we don't error out on first use
--- It is the same as local packer = require("packer") but this
--- way if it fails, it would return and not proceed downward
-local status_ok, packer = pcall(require, "packer")
+local status_ok, lazy = pcall(require, "lazy")
 if not status_ok then
   return
 end
 
 -- Install your plugins here
-return packer.startup(function(use)
-  use "wbthomason/packer.nvim"          -- Have packer manage itself
-  use "nvim-lua/popup.nvim"             -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim"           -- Useful lua functions used by lots of plugins
+return lazy.setup({
+    -- themes
+    {
+        "joshdick/onedark.vim",
+        lazy = true,
+    },
+    {
+        "rose-pine/neovim",
+        lazy = true,
+        name = "rose-pine"
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = true,
+        priority = 1000
+    },
+   "tpope/vim-commentary",
+   "tpope/vim-fugitive",
+   "tpope/vim-surround",
+   "tpope/vim-rhubarb",           -- Enable fugitie's GBrowse and other Github's completions such as issues, commit messages, collaborators, ...
+   "tpope/vim-repeat",
+   "tpope/vim-unimpaired",
+   "tmux-plugins/vim-tmux",       -- useful for tmux config mainly K and :make to source config
 
   -- colorschemes
   use "joshdick/onedark.vim"

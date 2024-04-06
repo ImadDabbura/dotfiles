e0313b7cbd4c938e4140827a200e8a52e5e4eaef
ImadDabbura <imad.dabbura@hotmail.com>
2024-04-05 19:07:01 -0500

    nvim: Restructure plugins

--- nvim/config/lua/user/plugins.lua
+++ nvim/config/lua/user/plugins.lua
@@ -1,93 +1,114 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
--- vim.cmd [[
---   augroup packer_user_config
---     autocmd!
---     autocmd BufWritePost plugins.lua source <afile> | PackerSync
---   augroup end
--- ]]
-
 local status_ok, lazy = pcall(require, "lazy")
 if not status_ok then
-  return
+	return
 end
 
--- Install your plugins here
 return lazy.setup({
-    -- themes
-    {
-        "joshdick/onedark.vim",
-        lazy = true,
-    },
-    {
-        "rose-pine/neovim",
-        lazy = true,
-        name = "rose-pine"
-    },
-    {
-        "folke/tokyonight.nvim",
-        lazy = true,
-        priority = 1000
-    },
-    {
-        "bluz71/vim-nightfly-colors",
-        name = "nightfly",
-        lazy = false,
-        priority = 1000
-    },
-   "tpope/vim-commentary",
-   "tpope/vim-fugitive",
-   "tpope/vim-surround",
-   "tpope/vim-rhubarb",           -- Enable fugitie's GBrowse and other Github's completions such as issues, commit messages, collaborators, ...
-   "tpope/vim-repeat",
-   "tpope/vim-unimpaired",
-   "tmux-plugins/vim-tmux",       -- useful for tmux config mainly K and :make to source config
+	-- themes
+	{
+		"joshdick/onedark.vim",
+		lazy = true,
+	},
+	{
+		"rose-pine/neovim",
+		lazy = true,
+		name = "rose-pine",
+	},
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			local bg = "#011628"
+			local bg_dark = "#011423"
+			local bg_highlight = "#143652"
+			local bg_search = "#0A64AC"
+			local bg_visual = "#275378"
+			local fg = "#CBE0F0"
+			local fg_dark = "#B4D0E9"
+			local fg_gutter = "#627E97"
+			local border = "#547998"
 
-    -- Revisit the following plugins for complete setup/understanding
-   "kshenoy/vim-signature",       -- Toggle & display marks (Revisit)
-    {
-     "folke/trouble.nvim",
-     dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate"
-    },
-    "folke/zen-mode.nvim",
-   "vim-scripts/ReplaceWithRegister",  -- Replace text block with register w/o contaminating the register
-   "mbbill/undotree",
-    {
-      "christoomey/vim-tmux-navigator",
-      cmd = {
-        "TmuxNavigateLeft",
-        "TmuxNavigateDown",
-        "TmuxNavigateUp",
-        "TmuxNavigateRight",
-        "TmuxNavigatePrevious",
-      },
-      keys = {
-        { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-        { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-        { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-        { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-        { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-      },
-    },
-   "nvim-lua/popup.nvim",             -- An implementation of the Popup API from vim in Neovim
-   "nvim-lua/plenary.nvim",           -- Useful lua functions used by lots of plugins
-   "junegunn/fzf",
-   "junegunn/fzf.vim",
-   "psf/black",
+			require("tokyonight").setup({
+				style = "night",
+				on_colors = function(colors)
+					colors.bg = bg
+					colors.bg_dark = bg_dark
+					colors.bg_float = bg_dark
+					colors.bg_highlight = bg_highlight
+					colors.bg_popup = bg_dark
+					colors.bg_search = bg_search
+					colors.bg_sidebar = bg_dark
+					colors.bg_statusline = bg_dark
+					colors.bg_visual = bg_visual
+					colors.border = border
+					colors.fg = fg
+					colors.fg_dark = fg_dark
+					colors.fg_float = fg
+					colors.fg_gutter = fg_gutter
+					colors.fg_sidebar = fg_dark
+				end,
+			})
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
+	{
+		"bluz71/vim-nightfly-colors",
+		name = "nightfly",
+		lazy = false,
+		priority = 1000,
+	},
+
+	-- Core
+	{
+		"numToStr/Comment.nvim",
+		lazy = false,
+	},
+	{
+		"kylechui/nvim-surround",
+		event = { "BufReadPre", "BufNewFile" },
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		config = true,
+	},
+	"tpope/vim-fugitive",
+	"tpope/vim-rhubarb", -- Enable fugitie's GBrowse and other Github's completions such as issues, commit messages, collaborators, ...
+	"tpope/vim-repeat",
+	"tpope/vim-unimpaired",
+	"tmux-plugins/vim-tmux", -- useful for tmux config mainly K and :make to source config
+	"kshenoy/vim-signature", -- Toggle & display marks (Revisit)
+	{
+		"folke/zen-mode.nvim",
+		keys = {
+			{ "<leader>sm", "<cmd>ZenMode<CR>", desc = "Maximize/minimize a split" },
+		},
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
 })

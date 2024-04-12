bc5a546d17e4a26d51af78f54f98dfed7a8616d5
ImadDabbura <imad.dabbura@hotmail.com>
2024-04-12 12:10:34 -0500

    nvim: Add telescope plugin

--- nvim/config/lua/user/plugins.lua
+++ nvim/config/lua/user/plugins.lua
@@ -284,5 +284,47 @@ return lazy.setup({
           },
         })
       end,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            local telescope = require("telescope")
+            local actions = require("telescope.actions")
+
+            telescope.setup({
+                defaults = {
+                    path_display = { "smart" },
+                    mappings = {
+                        i = {
+                            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+                            ["<C-j>"] = actions.move_selection_next, -- move to next result
+                            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                        },
+                    },
+                },
+            })
+
+            telescope.load_extension("fzf")
+
+            -- set keymaps
+            local keymap = vim.keymap -- for conciseness
+
+            keymap.set("n", "<leader>f", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+            keymap.set("n", "<leader>b", "<cmd>Telescope buffers<cr>", { desc = "Fuzzy find buffers" })
+            keymap.set("n", "<leader>r", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+            keymap.set("n", "<leader>s", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+            keymap.set("n", "<leader>c", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+            keymap.set("n", "<leader>ch", "<cmd>Telescope command_history<cr>", { desc = "Fuzzy find command history" })
+            keymap.set("n", "<leader>sh", "<cmd>Telescope search_history<cr>", { desc = "Fuzzy find search history" })
+            keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>", { desc = "Fuzzy find git status" })
+            keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>", { desc = "Fuzzy find git commits" })
+            keymap.set("n", "<leader>gbc", "<cmd>Telescope git_bcommits<cr>", { desc = "Fuzzy find buffer git commits" })
+        end,
     }
 })

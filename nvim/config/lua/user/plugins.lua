4b72d3f7e498ee05f10cf128022c2cbc3f0252c8
ImadDabbura <imad.dabbura@hotmail.com>
2024-04-11 20:25:34 -0500

    nvim: Add nvim greeter

--- nvim/config/lua/user/plugins.lua
+++ nvim/config/lua/user/plugins.lua
@@ -166,5 +166,41 @@ return lazy.setup({
         keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
         keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
       end
-    }
+    },
+    {
+      "goolord/alpha-nvim",
+      event = "VimEnter",
+      config = function()
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.dashboard")
+
+        -- Set header
+        dashboard.section.header.val = {
+          "                                                     ",
+          "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+          "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+          "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+          "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+          "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+          "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+          "                                                     ",
+        }
+
+        -- Set menu
+        dashboard.section.buttons.val = {
+          dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+          dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+          dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
+          dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+          dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+          dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
+        }
+
+        -- Send config to alpha
+        alpha.setup(dashboard.opts)
+
+        -- Disable folding on alpha buffer
+        vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+      end,
+    },
 })

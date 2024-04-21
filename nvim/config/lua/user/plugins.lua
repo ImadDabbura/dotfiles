bf9bbe3499fc43b0b98d3620fc7b8864186567ff
ImadDabbura <imad.dabbura@hotmail.com>
2024-04-20 20:40:36 -0500

    nvim: Add indent-blankline plugin

--- nvim/config/lua/user/plugins.lua
+++ nvim/config/lua/user/plugins.lua
@@ -504,4 +504,12 @@ return lazy.setup({
         })
       end,
     },
+    {
+      "lukas-reineke/indent-blankline.nvim",
+      event = { "BufReadPre", "BufNewFile" },
+      main = "ibl",
+      opts = {
+        indent = { char = "┊" },
+      },
+    },
 })
